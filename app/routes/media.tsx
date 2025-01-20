f7d91abec29b76bf497e5f4acd801b3fa1bc1d7c
Shawn Wilson <shawnwilsontrpt@gmail.com>
2025-01-20 09:49:29 -0500

    refreshed media page to masonry layout

--- app/routes/media.tsx
+++ app/routes/media.tsx
@@ -1,61 +1,25 @@
-import PhotoContainer from "~/components/PhotoContainer";
 import { mediaPagePhotos } from "~/data/photoData"
+import { FadeIn, FadeInStagger } from "~/components/FadeIn"
 
 export default function Media() {
-  const photos = mediaPagePhotos.reverse();
-  return (
-    <>
-    <div className="mb-8 grid grid-cols-2 md:grid-cols-4 gap-4">
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
-        </div>
-    </div>
-</div>
-      <div className="md:mt-12 sm:mt-16 lg:mt-2">
-        <PhotoContainer photos={photos} layout="grid" size="medium" filePath="media" photoStyle="cover"/>
-      </div>
+	const photos = mediaPagePhotos.reverse();
 
-      
-    </>
-  )
+	return (
+		<FadeInStagger>
+			<div className="px-4 sm:px-6 md:px-8 columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-4 mx-auto space-y-4">
+				{photos.map((photo, index) => (
+					<FadeIn key={index}>
+						<div className="break-inside-avoid">
+							<img
+								src={`assets/media/${photo.src}`}
+								alt={photo.alt}
+								className="w-full rounded-lg"
+								loading="lazy"
+							/>
+						</div>
+					</FadeIn>
+				))}
+			</div>
+		</FadeInStagger>
+	)
 }
