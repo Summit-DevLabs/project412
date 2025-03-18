dbb0ebe3683fe69aa93c237e6997b1b2ecf6ec06
Shawn Wilson <shawnwilsontrpt@gmail.com>
2025-03-17 20:33:21 -0400

    removed races from nav bar

--- app/data/headerButtonData.ts
+++ app/data/headerButtonData.ts
@@ -1,19 +1,20 @@
 export const headerButtonData = [
   {
-    id: 1,
-    link: 'roster',
-    title: 'Roster',
-  },
-  {
     id: 4,
     link: 'sponsors',
     title: 'Sponsors'
   },
   {
-    id: 2,
-    link: 'races',
-    title: 'Races'
+    id: 1,
+    link: 'roster',
+    title: 'Roster',
   },
+  
+  // {
+  //   id: 2,
+  //   link: 'races',
+  //   title: 'Races'
+  // },
   {
     id: 3,
     link: 'media',
