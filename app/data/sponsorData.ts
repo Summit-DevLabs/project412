0e05d7b55be9b0afaf44273ea73b0a0730be3ff4
Shawn Wilson <shawnwilsontrpt@gmail.com>
2025-03-17 20:26:11 -0400

    added url click to sponsor page

--- app/data/sponsorData.ts
+++ app/data/sponsorData.ts
@@ -1,4 +1,12 @@
-export const sponsorData = [
+interface Sponsor {
+  name: string;
+  img: string;
+  url: string;
+  description: string;
+  new: boolean;
+}
+
+export const sponsorData: Sponsor[] = [
   {
     name: "Visit Pittsburgh",
     img: "VP_Logo_1.jpg",
@@ -10,9 +18,8 @@ export const sponsorData = [
     name: "Rush 2 Crush Cancer",
     img: "r2c.svg",
     url: "https://www.rushtocrushcancer.org/",
-    description: "UPMC Hillman Cancer Center’s goal is life without cancer and Rush to Crush Cancer’s sole mission is to help achieve this goal with each push of the pedal.",
+    description: "UPMC Hillman Cancer Center's goal is life without cancer and Rush to Crush Cancer's sole mission is to help achieve this goal with each push of the pedal.",
     new: false,
-
   },
   {
     name: "Never Second",
@@ -20,7 +27,6 @@ export const sponsorData = [
     url: "https://never2.com/",
     description: "Science First. Neversecond.",
     new: false,
-
   },
   {
     name: "Abus",
@@ -28,7 +34,6 @@ export const sponsorData = [
     url: "https://www.abus.com/usa",
     description: "At ABUS, we have a steadfast commitment: your peace of mind. Our primary focus is to deliver security and safety solutions that guarantee peace of mind for your residence, business, or leisure activities. For nearly a century, we have been developing security solutions for homes, diverse industries, and individuals on the move.",
     new: false,
-
   },
   {
     name: "Silca",
@@ -36,7 +41,6 @@ export const sponsorData = [
     url: "silca.cc",
     description: "The world leader in quality bicycle accessories with a relentless pursuit of perfection. Now offering chain lube, bar tape, pumps, tools, packs and more.",
     new: false,
-
   },
   {
     name: "ATP Race Consulting",
@@ -44,7 +48,6 @@ export const sponsorData = [
     url: "https://www.atpraceconsulting.info/",
     description: "ATP Race Consulting is a cycling coaching service committed to making better cyclists of all levels. Whether you want to finish the local group ride or be at the front at a national championship, ATP Race Consulting can help you achieve top performance.",
     new: false,
-
   },
   {
     name: "Big Dog Coffee",
@@ -52,7 +55,6 @@ export const sponsorData = [
     url: "https://www.bigdogcoffeeshop.com/",
     description: "This quaint nook in a vintage building offers a variety of gourmet brews, light fare & desserts.",
     new: false,
-
   },
   {
     name: "Summit DevLabs",
