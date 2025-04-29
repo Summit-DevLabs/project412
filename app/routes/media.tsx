8cbab3a523ada8a112888b522abba7a3c2a0f5e3
Shawn Wilson <shawnwilsontrpt@gmail.com>
2025-04-29 09:05:04 -0400

    photo reverse on media

--- app/routes/media.tsx
+++ app/routes/media.tsx
@@ -2,12 +2,11 @@ import { mediaPagePhotos } from "~/data/photoData"
 import { FadeIn, FadeInStagger } from "~/components/FadeIn"
 
 export default function Media() {
-	const photos = mediaPagePhotos.reverse();
 
 	return (
 		<FadeInStagger>
 			<div className="px-4 sm:px-6 md:px-8 columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-4 mx-auto space-y-4">
-				{photos.map((photo, index) => (
+				{[...mediaPagePhotos].reverse().map((photo, index) => (
 					<FadeIn key={index}>
 						<div className="break-inside-avoid group relative">
 							<img
